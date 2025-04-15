7d7b32d6bf65624d19a5b89bc713a6c475224fd3
Caio Souza <souzsdev@gmail.com>
2025-04-14 23:32:54 -0300

    first commit

---
+++ src/test/java/com/souzs/scb/ScbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzs.scb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
